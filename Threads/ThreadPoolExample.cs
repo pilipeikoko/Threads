b43edec79ea7a9e06d4f3c5ce141afea99b322a3
pilipeikoko <pilipeikoko@gmail.com>
2021-08-02 16:23:37 +0300

    feat: example of thread pool

---
+++ Threads/ThreadPoolExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class ThreadPoolExample
+    {
+        public static void Main(string[] args)
+        {
+            int nWorkerThreads;
+            int nCompletionThreads = 2;
+
+            ThreadPool.GetMaxThreads(out nWorkerThreads, out nCompletionThreads);
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Print);
+            }
+
+            Thread.Sleep(3000);
+
+            Console.ReadLine();
+
+        }
+
+        static void Print(object state)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Cycle {i}, thread {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
