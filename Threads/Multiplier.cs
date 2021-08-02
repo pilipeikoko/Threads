e03b18fb842e3c61061531712e83b140024dc71a
pilipeikoko <pilipeikoko@gmail.com>
2021-08-02 11:34:27 +0300

    manual creation of threads and example if improving perfomance within it lock example

---
+++ Threads/Multiplier.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    public class Multiplier
+    {
+        int firstNumber;
+        int secondNumber;
+        public int rankToMultiply;
+        public int result;
+        public int Result
+        {
+            get
+            {
+                return result;
+            }
+        }
+
+
+        private object locker = new object();
+
+        public Multiplier(int firstNumber, int secondNumber)
+        {
+            this.firstNumber = firstNumber;
+            this.secondNumber = secondNumber;
+        }
+
+        public void Multiply()
+        {
+            int firstNumber;
+            int secondNumber;
+            int currentRank;
+
+            lock (locker)
+            {
+                firstNumber = this.firstNumber;
+                secondNumber = this.secondNumber;
+                currentRank = this.rankToMultiply++;
+            }
+
+
+            int numToMultiply = (secondNumber / (int)Math.Pow(10, currentRank)) % 10;
+            int tempResult = numToMultiply * firstNumber;
+            Interlocked.Add(ref result, tempResult * (int)Math.Pow(10, currentRank));
+        }
+    }
+}
--- Threads/Program.cs
+++ Threads/Program.cs
@@ -6,48 +6,6 @@ namespace Threads
 {
     public class Program
     {
-        private class Multiplier
-        {
-            int firstNumber;
-            int secondNumber;
-            public int rankToMultiply;
-            public int result;
-            public int Result {
-                get { 
-                    return result;
-                }
-            }
-
-
-            private object locker = new object();
-
-            public Multiplier(int firstNumber, int secondNumber)
-            {
-                this.firstNumber = firstNumber;
-                this.secondNumber = secondNumber;
-            }
-
-            public void Multiply()
-            {
-                int firstNumber;
-                int secondNumber;
-                int currentRank;
-
-                lock (locker)
-                {
-                    firstNumber = this.firstNumber;
-                    secondNumber = this.secondNumber;
-                    currentRank = this.rankToMultiply++;
-                }
-
-
-                int numToMultiply = (secondNumber / (int)Math.Pow(10, currentRank)) % 10;
-                int tempResult = numToMultiply * firstNumber;
-                Interlocked.Add(ref result, tempResult * (int)Math.Pow(10, currentRank));
-            }
-        }
-
-
         public static void Main(string[] args)
         {
             int firstNumber = int.Parse(Console.ReadLine());
@@ -66,6 +24,7 @@ namespace Threads
             thirdThread.Join();
             fourthThread.Join();
             Console.WriteLine(multiplier.Result);
+
         }
 
 
