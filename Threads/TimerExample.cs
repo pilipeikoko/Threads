da9fc3661d6c72fd459d1ab07b923664e8168d49
pilipeikoko <pilipeikoko@gmail.com>
2021-08-02 14:50:51 +0300

    feat: timer example

---
+++ Threads/TimerExample.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class TimerExample
+    {
+
+        public static void UpdateAndShowWeather(object obj)
+        {
+            WeatherService service = new();
+            service.UpdateWeather();
+            Console.WriteLine($"Temperature in {service.CityName}: {service.Temperature} °C, feels like {service.FeelsLike}");
+        }
+
+        public static void Main(string[] args)
+        {
+            TimerCallback timerCallback = new TimerCallback(UpdateAndShowWeather);
+            Timer timer = new Timer(timerCallback, null, 0, 1000);
+            Console.ReadLine();
+        }
+    }
+}
