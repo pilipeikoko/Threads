72bc5e28889fe8fb39a4c8444d7d6c8c3e2c89a5
pilipeikoko <pilipeikoko@gmail.com>
2021-08-03 11:15:57 +0300

    async and await examples

---
+++ Threads/AsyncExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class AsyncExample
+    {
+        public static void Main(string[] args)
+        {
+            Task<int> b = PrintAsync();
+          //  Console.WriteLine(b.Result);
+            for(int i = 100; i < 200; ++i)
+            {
+                Console.WriteLine(1);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static async Task<int> PrintAsync()
+        {
+            
+            return await Task.Run(PrintSomething);
+            
+        }
+
+        public static int PrintSomething() { 
+            for(int i = 0; i < 100; ++i)
+            {
+                Console.WriteLine("A");
+            }
+            return 20;
+        }
+
+    }
+}
